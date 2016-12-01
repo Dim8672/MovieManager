3f7fb6f257627b5c9f4f2a930b962e49ab810685
Dim8672 <dimitri.mella@gmail.com>
2016-12-02 00:22:23 +0100

    Initialisation of different main project files

---
+++ src/main/java/ch/hearc/ig/odi/moviemanager/business/Movie.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.moviemanager.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author dimitri.mella
+ */
+public class Movie {
+
+    private Long id;
+    private String name;
+    private String producer;
+    private List<Person> people;
+
+    public Movie() {
+
+    }
+
+    public Movie(Long id, String name, String producer) {
+        this.id = id;
+        this.name = name;
+        this.producer = producer;
+        this.people = people;
+        this.people = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public List<Person> getPeople() {
+        return people;
+    }
+
+}
